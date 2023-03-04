703c7469950ec4be96103a78bf87fe4b8f6f5ce9
ahmettolay <101968628+ahmettolay@users.noreply.github.com>
2023-03-04 05:06:11 +0300

    Revert "orkun bozuk kodu"
    This reverts commit adc3e90e275167f554309388d1960decab8ffadd.

--- src/components/Card/BlogCard.js
+++ src/components/Card/BlogCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 import './BlogCard.css'
 
-
 const BlogCard=()=>{
     return(
         <>
   <title>Blog card</title>
   <div className="container">
     <div className="square">
-      {/* <img
+      <img
         src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80"
         className="mask"
-      /> */}
+      />
       <div className="h1">Is Apple a Design Company?</div>
       <p>
         Apple is more than a tech company; it became a culture unto itself, a
