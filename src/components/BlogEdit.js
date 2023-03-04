fdf4d788503e78d11f1ec21f5501edade763baef
ahmettolay <101968628+ahmettolay@users.noreply.github.com>
2023-03-04 04:46:16 +0300

    img , refactor kod

--- src/components/BlogEdit.js
+++ src/components/BlogEdit.js
@@ -1,33 +1,50 @@
-import { useContext, useState } from "react"
-import BlogContext from "../context/blogs"
+import { useContext, useState } from "react";
+import BlogContext from "../context/blogs";
 
 function BlogEdit({ blog, onSubmit }) {
-    const {editBlogById}=useContext(BlogContext)
-    const [title,setTitle]=useState(blog.title)
-    const [body,setBody]=useState(blog.body)
-const handleChangeTitle =(e)=>{
-setTitle(e.target.value)
-}
-const handleChangeBody =(e)=>{
-  setBody(e.target.value)
-  }
-const handleSubmit = (event)=>{
-  event.preventDefault()
-    editBlogById(blog.id,title,body)
-    onSubmit()
-}
+  const { editBlogById } = useContext(BlogContext);
+  const [title, setTitle] = useState(blog.title);
+  const [body, setBody] = useState(blog.body);
+  const [img, setImg] = useState(blog.img);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    editBlogById(blog.id, title, body, img);
+    onSubmit();
+  };
 
   return (
     <form className="blog-edit" onSubmit={handleSubmit}>
-<label >Title</label>
-<input className="input" value={title} onChange={handleChangeTitle} />
-<label >Body</label>
-<input className="input" value={body} onChange={handleChangeBody} />
-<button className='button is-primary'>Save</button>
+      <div>
+        <label>Title:</label>
+        <input
+          className="input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Img:</label>
+        <input
+          className="input"
+          value={img}
+          onChange={(e) => setImg(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Body:</label>
+        <input
+          className="input"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+
+
+      <button className="button is-primary">Save</button>
     </form>
-
-
-  )
+  );
 }
 
-export default BlogEdit
+export default BlogEdit;
