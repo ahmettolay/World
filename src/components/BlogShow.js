2f03ad306960f9b49204b107f2d3d3ecb3c4477d
OrkunXY <63229503+OrkunXY@users.noreply.github.com>
2023-03-05 23:40:00 +0300

    show-more buton eklendi

--- src/components/BlogShow.js
+++ src/components/BlogShow.js
@@ -1,13 +1,18 @@
 import { useState, useContext } from "react";
 import BlogEdit from "./BlogEdit";
 import BlogContext from "../context/blogs";
-import '../index.css'
-import './Card/BlogCard.css'
-
+import "../index.css";
+import "./Card/BlogCard.css";
 
 function BlogShow({ blog }) {
   const { deleteBlogById } = useContext(BlogContext);
   const [showEdit, setShowEdit] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
   const handleDeleteClick = () => {
     deleteBlogById(blog.id);
   };
@@ -18,34 +23,44 @@ function BlogShow({ blog }) {
   const handleSubmit = () => {
     setShowEdit(false);
   };
-  let content = (
-   
-    <div className="container">
-        <div className="square">
-          <img src={blog.img} alt={blog.title} className="mask"/>
-          <h1 className="h1">{blog.title}</h1>
-          <p>{blog.body}</p>
-          <p>{blog.blogCreateDate}</p>
-          <button className="button"  onClick={handleEditClick}>
-            <img src="" alt="" />
-            Edit
-          </button>
-          <button className="button"  onClick={handleDeleteClick}>
-            Delete
-          </button>
-      </div>
-      </div>
-    
   
+  let content = (
+    <div className="container">
+      <div className="square">
+        <img src={blog.img} alt={blog.title} className="mask" />
+        <h1 className="h1">{blog.title}</h1>
+        {showMore ? (
+          <p className="blogBody">{blog.body}</p>
+        ) : (
+          <p className="blogBody">{blog.body.slice(0, 100)}</p>
+        )}
+        {!showMore && (
+          <button className="show-more-btn" onClick={toggleShowMore}>
+            More
+          </button>
+        )}
+        {showMore && (
+          <button className="button" onClick={toggleShowMore}>
+            Less
+          </button>
+        )}
+        <p>{blog.blogCreateDate}</p>
+        <button className="edit-btn" onClick={handleEditClick}>
+          <img src="" alt="" />
+          Edit
+        </button>
+        <button className="delete-btn" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
   );
 
   if (showEdit) {
     content = <BlogEdit onSubmit={handleSubmit} blog={blog} />;
   }
 
-  return <div className="deneme">{content}</div>
-    
-    
-   
+  return <div className="deneme">{content}</div>;
 }
+
 export default BlogShow;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,25 @@ import {  Link } from "react-router-dom";
 
 const navbar= () =>{
   return (
-  <div>
-    <li>
-      <a href='/components/Page/Blog'>Blog</a>
-      {/* <href"./components/Page/Blog">Dogs</href> */}
+  <nav className='navbar'>
+    <ul className='nav-menu'>
+    <li className='nav-item'>
+    <a href='/components/Page/HomePage'className='AnaSayfa'>Ana Sayfa</a>
     </li>
-    <li>
-    <a href='/components/Page/HomePage'>Ana Sayfa</a>
-    </li>
-    <li>
-    <a href='/components/Page/Info'>Hakkımızda</a>
-    </li>
-    <li>
+    <li className='nav-item'>
     <a href='/components/Page/Educations'>Yurt Dışı Eğitimler</a>
     </li>
-  </div>
+    <li className='nav-item'>
+      <a href='/components/Page/Blog' className='blog'>Blog</a>
+    </li>
+    
+    <li className='nav-item'>
+    <a href='/components/Page/Info'>Hakkımızda</a>
+    </li>
+    
+    </ul>
+  </nav>
+
   );
 }
 export default navbar;
